d32c5179190c959d33aad48b746cf9dddf63aba6
Pratham Soni <prathamsoni@Prathams-MacBook-Air.local>
2026-04-05 15:28:18 +0530

    Chapter 5.2, Stripe Payments added

---
+++ src/main/java/com/prathamsoni/projects/lovable_clone/repository/PlanRepository.java
@@ -0,0 +1,7 @@
+package com.prathamsoni.projects.lovable_clone.repository;
+
+import com.prathamsoni.projects.lovable_clone.entity.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+}
--- src/main/java/com/prathamsoni/projects/lovable_clone/service/SubscriptionService.java
+++ src/main/java/com/prathamsoni/projects/lovable_clone/service/SubscriptionService.java
@@ -1,14 +1,7 @@
 package com.prathamsoni.projects.lovable_clone.service;
 
-import com.prathamsoni.projects.lovable_clone.dto.subscription.CheckoutRequest;
-import com.prathamsoni.projects.lovable_clone.dto.subscription.CheckoutResponse;
-import com.prathamsoni.projects.lovable_clone.dto.subscription.PortalResponse;
 import com.prathamsoni.projects.lovable_clone.dto.subscription.SubscriptionResponse;
 
 public interface SubscriptionService {
     SubscriptionResponse getCurrentSubscription(Long userId);
-
-    CheckoutResponse createCheckoutSessionUrl(CheckoutRequest request, Long userId);
-
-    PortalResponse openCustomerPortal(Long userId);
 }
